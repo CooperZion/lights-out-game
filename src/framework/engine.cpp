4ea6ebf2cf51e3090e3c355c3ddee900db64f6d9
Cooper Zion <cooperzion2002@gmail.com>
2023-11-12 12:12:08 -0500

    Added necessary OpenGL files

---
+++ src/framework/engine.cpp
@@ -0,0 +1,99 @@
+#include "engine.h"
+
+const color WHITE(1, 1, 1);
+const color BLACK(0, 0, 0);
+const color BLUE(0, 0, 1);
+const color RED(1, 0, 0);
+const color GREEN(0, 1, 1);
+
+Engine::Engine() {
+    this->initWindow();
+    this->initShaders();
+    this->initShapes();
+}
+
+Engine::~Engine() {}
+
+unsigned int Engine::initWindow(bool debug) {
+    // glfw: initialize and configure
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_COCOA_RETINA_FRAMEBUFFER, GLFW_FALSE);
+#endif
+    glfwWindowHint(GLFW_RESIZABLE, false);
+
+    window = glfwCreateWindow(WIDTH, HEIGHT, "engine", nullptr, nullptr);
+    glfwMakeContextCurrent(window);
+
+    // glad: load all OpenGL function pointers
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        cout << "Failed to initialize GLAD" << endl;
+        return -1;
+    }
+
+    // OpenGL configuration
+    glViewport(0, 0, WIDTH, HEIGHT);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glfwSwapInterval(1);
+
+    return 0;
+}
+
+void Engine::initShaders() {
+    shaderManager = make_unique<ShaderManager>();
+    shapeShader = this->shaderManager->loadShader("../res/shaders/circle.vert",
+                                                  "../res/shaders/circle.frag",
+                                                  nullptr, "circle");
+    shapeShader.use();
+    shapeShader.setMatrix4("projection", this->PROJECTION);
+}
+
+void Engine::initShapes() {
+}
+
+void Engine::processInput() {
+    glfwPollEvents();
+
+    // Close window if escape key is pressed
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+
+    // Mouse position saved to check for collisions
+    glfwGetCursorPos(window, &mouseX, &mouseY);
+    mouseY = HEIGHT - mouseY; // make sure mouse y-axis isn't flipped
+}
+
+
+void Engine::update() {
+    // Change values of objects
+    // Calculate delta time
+    float currentFrame = glfwGetTime();
+    deltaTime = currentFrame - lastFrame;
+    lastFrame = currentFrame;
+
+
+
+}
+
+void Engine::render() {
+    // Draw objects
+    glClearColor(BLACK.red, BLACK.green, BLACK.blue, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    shapeShader.use();
+
+
+
+    glfwSwapBuffers(window);
+}
+
+bool Engine::shouldClose() {
+    return glfwWindowShouldClose(window);
+}
